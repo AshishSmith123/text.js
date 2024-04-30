9f9ef3983dcf52444d3f6b5fcc271c6ff780521f
AshishSmith123 <168326444+AshishSmith123@users.noreply.github.com>
2024-04-30 09:43:24 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as new version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3    
+    +3) // Do not do this , code readiability should be high 
+    console.log("Ashish")
+
+    let name = "Ashish"
+    let age = 17
+    let isLoggedin = false
+    let state = null;
+
+    // if number => 2^53
+    // we'll use => bigint
+    //string => ""
+    //boolean => true/false
+    //null => stand alnoe value
+    //undefined => representation of empty value
+    //symbol => unique
+
+    //object
+
+    console.log(typeof undefined)
